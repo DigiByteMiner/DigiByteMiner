b449e60e67cf3686718362db570e7b34dabacaf7
arunsatyarth <arun.satyarth@gmail.com>
2018-02-24 18:21:52 +0530

    added layout changes

--- DigibyteMiner/View/v1/Home.Designer.cs
+++ DigibyteMiner/View/v1/Home.Designer.cs
@@ -33,14 +33,18 @@
             this.lblShares = new System.Windows.Forms.Label();
             this.lblTotalHashrate = new System.Windows.Forms.Label();
             this.pbTemplate = new System.Windows.Forms.PictureBox();
-            this.lblMinerType = new System.Windows.Forms.Label();
             this.lblMinername = new System.Windows.Forms.Label();
             this.lnlMainCoinPool = new System.Windows.Forms.LinkLabel();
-            this.lnDualCoinPool = new System.Windows.Forms.LinkLabel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbTemplate)).BeginInit();
-            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblMinerState
@@ -49,7 +53,7 @@
             this.lblMinerState.BackColor = System.Drawing.Color.White;
             this.lblMinerState.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblMinerState.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.lblMinerState.Location = new System.Drawing.Point(21, 154);
+            this.lblMinerState.Location = new System.Drawing.Point(44, 161);
             this.lblMinerState.Name = "lblMinerState";
             this.lblMinerState.Size = new System.Drawing.Size(54, 15);
             this.lblMinerState.TabIndex = 33;
@@ -58,7 +62,7 @@
             // btnStartMining
             // 
             this.btnStartMining.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnStartMining.Location = new System.Drawing.Point(22, 178);
+            this.btnStartMining.Location = new System.Drawing.Point(530, 202);
             this.btnStartMining.Name = "btnStartMining";
             this.btnStartMining.Size = new System.Drawing.Size(53, 25);
             this.btnStartMining.TabIndex = 32;
@@ -69,7 +73,7 @@
             // 
             this.lblShares.AutoSize = true;
             this.lblShares.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblShares.Location = new System.Drawing.Point(106, 188);
+            this.lblShares.Location = new System.Drawing.Point(499, 106);
             this.lblShares.Name = "lblShares";
             this.lblShares.Size = new System.Drawing.Size(84, 13);
             this.lblShares.TabIndex = 31;
@@ -79,7 +83,7 @@
             // 
             this.lblTotalHashrate.AutoSize = true;
             this.lblTotalHashrate.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTotalHashrate.Location = new System.Drawing.Point(205, 121);
+            this.lblTotalHashrate.Location = new System.Drawing.Point(492, 52);
             this.lblTotalHashrate.Name = "lblTotalHashrate";
             this.lblTotalHashrate.Size = new System.Drawing.Size(91, 25);
             this.lblTotalHashrate.TabIndex = 30;
@@ -88,28 +92,18 @@
             // pbTemplate
             // 
             this.pbTemplate.Image = global::DigibyteMiner.Properties.Resources.digibyte;
-            this.pbTemplate.Location = new System.Drawing.Point(24, 40);
+            this.pbTemplate.Location = new System.Drawing.Point(23, 25);
             this.pbTemplate.Name = "pbTemplate";
-            this.pbTemplate.Size = new System.Drawing.Size(65, 63);
+            this.pbTemplate.Size = new System.Drawing.Size(91, 94);
             this.pbTemplate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbTemplate.TabIndex = 28;
             this.pbTemplate.TabStop = false;
-            // 
-            // lblMinerType
-            // 
-            this.lblMinerType.AutoSize = true;
-            this.lblMinerType.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMinerType.Location = new System.Drawing.Point(106, 74);
-            this.lblMinerType.Name = "lblMinerType";
-            this.lblMinerType.Size = new System.Drawing.Size(68, 13);
-            this.lblMinerType.TabIndex = 27;
-            this.lblMinerType.Text = "DefaultType";
             // 
             // lblMinername
             // 
             this.lblMinername.AutoSize = true;
             this.lblMinername.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMinername.Location = new System.Drawing.Point(105, 40);
+            this.lblMinername.Location = new System.Drawing.Point(263, 90);
             this.lblMinername.Name = "lblMinername";
             this.lblMinername.Size = new System.Drawing.Size(97, 20);
             this.lblMinername.TabIndex = 23;
@@ -121,7 +115,7 @@
             this.lnlMainCoinPool.LinkArea = new System.Windows.Forms.LinkArea(0, 15);
             this.lnlMainCoinPool.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
             this.lnlMainCoinPool.LinkColor = System.Drawing.Color.RoyalBlue;
-            this.lnlMainCoinPool.Location = new System.Drawing.Point(8, 21);
+            this.lnlMainCoinPool.Location = new System.Drawing.Point(403, 210);
             this.lnlMainCoinPool.Name = "lnlMainCoinPool";
             this.lnlMainCoinPool.Size = new System.Drawing.Size(55, 17);
             this.lnlMainCoinPool.TabIndex = 19;
@@ -129,62 +123,122 @@
             this.lnlMainCoinPool.Text = "Main Pool";
             this.lnlMainCoinPool.UseCompatibleTextRendering = true;
             // 
-            // lnDualCoinPool
-            // 
-            this.lnDualCoinPool.AutoSize = true;
-            this.lnDualCoinPool.LinkArea = new System.Windows.Forms.LinkArea(0, 15);
-            this.lnDualCoinPool.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
-            this.lnDualCoinPool.LinkColor = System.Drawing.Color.RoyalBlue;
-            this.lnDualCoinPool.Location = new System.Drawing.Point(101, 21);
-            this.lnDualCoinPool.Name = "lnDualCoinPool";
-            this.lnDualCoinPool.Size = new System.Drawing.Size(53, 17);
-            this.lnDualCoinPool.TabIndex = 21;
-            this.lnDualCoinPool.TabStop = true;
-            this.lnDualCoinPool.Text = "Dual Pool";
-            this.lnDualCoinPool.UseCompatibleTextRendering = true;
-            this.lnDualCoinPool.Visible = false;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.lnlMainCoinPool);
-            this.groupBox1.Controls.Add(this.lnDualCoinPool);
-            this.groupBox1.Location = new System.Drawing.Point(75, 349);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(162, 44);
-            this.groupBox1.TabIndex = 34;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Visit Pool Account";
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(356, 40);
+            this.label1.Location = new System.Drawing.Point(160, 121);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(78, 20);
             this.label1.TabIndex = 35;
             this.label1.Text = "Algorithm";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(140, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 25);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "Digibyte Miner";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(160, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Algorithm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(160, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 38;
+            this.label4.Text = "Algorithm";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(244, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 20);
+            this.label5.TabIndex = 39;
+            this.label5.Text = ":";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(244, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 20);
+            this.label6.TabIndex = 40;
+            this.label6.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(244, 141);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 20);
+            this.label7.TabIndex = 41;
+            this.label7.Text = ":";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(151, 90);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 20);
+            this.label8.TabIndex = 42;
+            this.label8.Text = "Algorithm";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(244, 90);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 20);
+            this.label9.TabIndex = 43;
+            this.label9.Text = ":";
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1129, 465);
+            this.Controls.Add(this.lnlMainCoinPool);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lblMinerState);
             this.Controls.Add(this.btnStartMining);
             this.Controls.Add(this.lblShares);
             this.Controls.Add(this.lblTotalHashrate);
             this.Controls.Add(this.pbTemplate);
-            this.Controls.Add(this.lblMinerType);
             this.Controls.Add(this.lblMinername);
-            this.Controls.Add(this.groupBox1);
             this.Name = "Home";
             this.Text = "Home";
             this.Load += new System.EventHandler(this.Home_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pbTemplate)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,11 +251,16 @@
         private System.Windows.Forms.Label lblShares;
         private System.Windows.Forms.Label lblTotalHashrate;
         private System.Windows.Forms.PictureBox pbTemplate;
-        private System.Windows.Forms.Label lblMinerType;
         private System.Windows.Forms.Label lblMinername;
         private System.Windows.Forms.LinkLabel lnlMainCoinPool;
-        private System.Windows.Forms.LinkLabel lnDualCoinPool;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
